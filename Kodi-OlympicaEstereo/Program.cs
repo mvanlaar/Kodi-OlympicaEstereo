94bf9664d40363eead642cdb543d49dc57f8e0f3
mvanlaar <martijn@mvanlaar.net>
2016-05-09 21:31:07 +0200

    Downloading url part working

--- Kodi-OlympicaEstereo/Program.cs
+++ Kodi-OlympicaEstereo/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,30 +11,87 @@ namespace Kodi_OlympicaEstereo
     class Program
     {
 
-        public static List<OlympicaUrls> _OlympicaUrls = new List<OlympicaUrls>
-    {
-        new OlympicaUrls {Url = "http://olimpicastereo.com.co/wp-pics/themes/olimpica/changeCity.php?city=7&amp;url_to_go=http%3A%2F%2Folimpicastereo.com.co%2F", City = "Armenia"},
-        new OlympicaUrls {Url = "http://olimpicastereo.com.co/wp-pics/themes/olimpica/changeCity.php?city=6&amp;url_to_go=http%3A%2F%2Folimpicastereo.com.co%2F", City = "Barranquilla"},
-        new OlympicaUrls {Url = "http://olimpicastereo.com.co/wp-pics/themes/olimpica/changeCity.php?city=8&amp;url_to_go=http%3A%2F%2Folimpicastereo.com.co%2F", City = "Bogotá"},
-        new OlympicaUrls {Url = "http://olimpicastereo.com.co/wp-pics/themes/olimpica/changeCity.php?city=9&amp;url_to_go=http%3A%2F%2Folimpicastereo.com.co%2F", City = "Bucaramanga"},
-        new OlympicaUrls {Url = "http://olimpicastereo.com.co/wp-pics/themes/olimpica/changeCity.php?city=10&amp;url_to_go=http%3A%2F%2Folimpicastereo.com.co%2F", City = "Cali"},
-        new OlympicaUrls {Url = "http://olimpicastereo.com.co/wp-pics/themes/olimpica/changeCity.php?city=11&amp;url_to_go=http%3A%2F%2Folimpicastereo.com.co%2F", City = "Cartagena"},
-        new OlympicaUrls {Url = "http://olimpicastereo.com.co/wp-pics/themes/olimpica/changeCity.php?city=12&amp;url_to_go=http%3A%2F%2Folimpicastereo.com.co%2F", City = "Cucuta"},
-        new OlympicaUrls {Url = "http://olimpicastereo.com.co/wp-pics/themes/olimpica/changeCity.php?city=13&amp;url_to_go=http%3A%2F%2Folimpicastereo.com.co%2F", City = "Ibagué"},
-        new OlympicaUrls {Url = "http://olimpicastereo.com.co/wp-pics/themes/olimpica/changeCity.php?city=14&amp;url_to_go=http%3A%2F%2Folimpicastereo.com.co%2F", City = "Manizales"},
-        new OlympicaUrls {Url = "http://olimpicastereo.com.co/wp-pics/themes/olimpica/changeCity.php?city=15&amp;url_to_go=http%3A%2F%2Folimpicastereo.com.co%2F", City = "Medellin"},
-        new OlympicaUrls {Url = "http://olimpicastereo.com.co/wp-pics/themes/olimpica/changeCity.php?city=16&amp;url_to_go=http%3A%2F%2Folimpicastereo.com.co%2F", City = "Monteria"},
-        new OlympicaUrls {Url = "http://olimpicastereo.com.co/wp-pics/themes/olimpica/changeCity.php?city=17&amp;url_to_go=http%3A%2F%2Folimpicastereo.com.co%2F", City = "Neiva"},
-        new OlympicaUrls {Url = "http://olimpicastereo.com.co/wp-pics/themes/olimpica/changeCity.php?city=18&amp;url_to_go=http%3A%2F%2Folimpicastereo.com.co%2F", City = "Pereira"},
-        new OlympicaUrls {Url = "http://olimpicastereo.com.co/wp-pics/themes/olimpica/changeCity.php?city=19&amp;url_to_go=http%3A%2F%2Folimpicastereo.com.co%2F", City = "Santa Marta"},
-        new OlympicaUrls {Url = "http://olimpicastereo.com.co/wp-pics/themes/olimpica/changeCity.php?city=20&amp;url_to_go=http%3A%2F%2Folimpicastereo.com.co%2F", City = "Sincelejo"},
-        new OlympicaUrls {Url = "http://olimpicastereo.com.co/wp-pics/themes/olimpica/changeCity.php?city=21&amp;url_to_go=http%3A%2F%2Folimpicastereo.com.co%2F", City = "Valledupar"},
-        new OlympicaUrls {Url = "http://olimpicastereo.com.co/wp-pics/themes/olimpica/changeCity.php?city=22&amp;url_to_go=http%3A%2F%2Folimpicastereo.com.co%2F", City = "Villavicencio"}
-    };
+        
         static void Main(string[] args)
         {
+            List<OlympicaUrls> _OlympicaUrls = new List<OlympicaUrls>
+            {
+                new OlympicaUrls {Url = "http://olimpicastereo.com.co/wp-pics/themes/olimpica/changeCity.php?city=7", City = "Armenia"},
+                new OlympicaUrls {Url = "http://olimpicastereo.com.co/wp-pics/themes/olimpica/changeCity.php?city=6", City = "Barranquilla"},
+                new OlympicaUrls {Url = "http://olimpicastereo.com.co/wp-pics/themes/olimpica/changeCity.php?city=8", City = "Bogotá"},
+                new OlympicaUrls {Url = "http://olimpicastereo.com.co/wp-pics/themes/olimpica/changeCity.php?city=9", City = "Bucaramanga"},
+                new OlympicaUrls {Url = "http://olimpicastereo.com.co/wp-pics/themes/olimpica/changeCity.php?city=10", City = "Cali"},
+                new OlympicaUrls {Url = "http://olimpicastereo.com.co/wp-pics/themes/olimpica/changeCity.php?city=11", City = "Cartagena"},
+                new OlympicaUrls {Url = "http://olimpicastereo.com.co/wp-pics/themes/olimpica/changeCity.php?city=12", City = "Cucuta"},
+                new OlympicaUrls {Url = "http://olimpicastereo.com.co/wp-pics/themes/olimpica/changeCity.php?city=13", City = "Ibagué"},
+                new OlympicaUrls {Url = "http://olimpicastereo.com.co/wp-pics/themes/olimpica/changeCity.php?city=14", City = "Manizales"},
+                new OlympicaUrls {Url = "http://olimpicastereo.com.co/wp-pics/themes/olimpica/changeCity.php?city=15", City = "Medellin"},
+                new OlympicaUrls {Url = "http://olimpicastereo.com.co/wp-pics/themes/olimpica/changeCity.php?city=16", City = "Monteria"},
+                new OlympicaUrls {Url = "http://olimpicastereo.com.co/wp-pics/themes/olimpica/changeCity.php?city=17", City = "Neiva"},
+                new OlympicaUrls {Url = "http://olimpicastereo.com.co/wp-pics/themes/olimpica/changeCity.php?city=18", City = "Pereira"},
+                new OlympicaUrls {Url = "http://olimpicastereo.com.co/wp-pics/themes/olimpica/changeCity.php?city=19", City = "Santa Marta"},
+                new OlympicaUrls {Url = "http://olimpicastereo.com.co/wp-pics/themes/olimpica/changeCity.php?city=20", City = "Sincelejo"},
+                new OlympicaUrls {Url = "http://olimpicastereo.com.co/wp-pics/themes/olimpica/changeCity.php?city=21", City = "Valledupar"},
+                new OlympicaUrls {Url = "http://olimpicastereo.com.co/wp-pics/themes/olimpica/changeCity.php?city=22", City = "Villavicencio"}
+            };
+
+
             // Loop through list
-            // Add to SRTM file
+            foreach (OlympicaUrls OlympicaUrls in _OlympicaUrls)
+            {
+                string path = AppDomain.CurrentDomain.BaseDirectory + "data\\ac-timetable-en.pdf";
+                Uri url = new Uri(OlympicaUrls.Url);
+                const string ua = "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64; Trident/5.0)";
+                const string referer = "http://olimpicastereo.com.co/";
+
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+                System.Net.WebRequest.DefaultWebProxy = null;                
+                request.UserAgent = ua;
+                request.Referer = referer;
+                request.CookieContainer = new CookieContainer();
+                System.Net.WebRequest.DefaultWebProxy = null;
+                request.Proxy = System.Net.WebRequest.DefaultWebProxy;
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+                if (response.StatusCode == HttpStatusCode.OK)
+                {
+                    Stream receiveStream = response.GetResponseStream();
+                    //StreamReader readStream = null;
+
+                    //if (response.CharacterSet == null)
+                    //{
+                    //    readStream = new StreamReader(receiveStream);
+                    //}
+                    //else
+                    //{
+                    //    readStream = new StreamReader(receiveStream, Encoding.GetEncoding(response.CharacterSet));
+                    //}
+                    string data = null;
+
+                    using (BufferedStream buffer = new BufferedStream(receiveStream))
+                    {
+                        using (StreamReader reader = new StreamReader(buffer))
+                        {
+                            data = reader.ReadToEnd();
+                        }
+                    }
+
+                   
+
+                    response.Close();
+                    //readStream.Close();
+
+                    int first = data.IndexOf("var response_station = '") + "var response_station = '".Length;
+                    int last = data.LastIndexOf("?type=.flv';");
+                    string streamurl = data.Substring(first, last - first);
+                    Console.WriteLine("Streamurl: {0} - City: {1}", streamurl, OlympicaUrls.City);
+                    
+
+                }
+                 // Add to SRTM file
+                
+            }
+               Console.ReadLine();
         }
     
     }
